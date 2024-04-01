24cd1e6acbb5e10e6a92161dff15905f6f24f290
LPCBCN044 <alexmrtc@gmail.com>
2024-04-01 12:59:07 +0200

    Create checkDniLength function

--- tests/dni.test.js
+++ tests/dni.test.js
@@ -1,10 +1,21 @@
 import { test, expect, describe } from "vitest";
+import { isValidDni, checkDniLength } from "./dni";
 
-describe("isValidDni(dni)", () => {
-    test("isValidDni(123456789T) should return error, dni too long", () => {
+describe("checkDniLength(dni)", () => {
+    test("checkDniLength(123456789T) should return error, dni too long", () => {
+        const dni = "123456789T";
+        const expectedResult = "Dni too long";
 
+        let lengthValidationResult = checkDniLength(dni);
+
+        expect(lengthValidationResult).toBe(expectedResult);
     }),
-    test("isValidDni(1234567T) should return error, dni too short", () => {
+    test("checkDniLength(1234567T) should return error, dni too short", () => {
+        const dni = "1234567T";
+        const expectedResult = "Dni too short";
 
+        let lengthValidationResult = checkDniLength(dni);
+
+        expect(lengthValidationResult).toBe(expectedResult);
     })
 })
