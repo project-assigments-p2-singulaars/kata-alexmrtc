b7455d37f479e711d2401048843d570fa63a544a
LPCBCN044 <alexmrtc@gmail.com>
2024-04-01 12:32:35 +0200

    Create first tests, length check

---
+++ tests/dni.test.js
@@ -0,0 +1,10 @@
+import { test, expect, describe } from "vitest";
+
+describe("isValidDni(dni)", () => {
+    test("isValidDni(123456789T) should return error, dni too long", () => {
+
+    }),
+    test("isValidDni(1234567T) should return error, dni too short", () => {
+
+    })
+})
